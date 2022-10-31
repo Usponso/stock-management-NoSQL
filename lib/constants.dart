7ad538b18d8dcd60be37768f87e3e549d2095964
Enzo Averlant <enaverlant@gmail.com>
2022-10-31 11:16:34 +0100

    :sparkles: feat: Adding card widget

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,1 +1,2 @@
 const API_URL = "https://flutter-stock-management.herokuapp.com";
+// const API_URL = "http://localhost:3000";
--- lib/screen/searchResultPage/SearchResultPage.dart
+++ lib/screen/searchResultPage/SearchResultPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:stock_management/screen/widgets/Cards/ProductCard.dart';
 
 import '../../modal/Device.dart';
 import '../../providers/devicesProvider.dart';
@@ -39,46 +40,7 @@ class SearchResultPage extends StatelessWidget {
                       itemCount: 1,
                       itemBuilder: (context, index) {
                         Device device = snapshot.data![index];
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                              side: BorderSide(
-                                  color: Colors.grey.shade500, width: 0.4)),
-                          elevation: 0,
-                          margin: EdgeInsets.all(10),
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          child: Padding(
-                            padding: const EdgeInsets.all(30),
-                            child: Column(
-                              children: [
-                                ConstrainedBox(
-                                  constraints: const BoxConstraints(
-                                    maxHeight: 200,
-                                  ),
-                                  child: Image.network(
-                                    device.picture,
-                                    fit: BoxFit.contain,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: 16,
-                                ),
-                                Column(children: [
-                                  Text(
-                                    device.name,
-                                    style:
-                                        TextStyle(fontWeight: FontWeight.bold),
-                                  ),
-                                  Text(
-                                      'Numéro de série : ${device.serialNumber}'),
-                                  Text('Prix : ${device.price} €'),
-                                  Text(
-                                      'Quantité en stock : ${device.stockQuantity}'),
-                                ])
-                              ],
-                            ),
-                          ),
-                        );
+                        return ProductCard(device: device);
                       });
                 } else {
                   return Center(
